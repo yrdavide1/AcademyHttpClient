e870e1187449ecdfdd03cd2e7b9526e43e0a9487
yrdavide1 <davicatra@gmail.com>
2021-10-27 13:31:21 +0200

    added /Model folder, started working on GUI

---
+++ AcademyHttpClient/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyHttpClient.Model
+{
+    public class Student : Person
+    {
+        public bool IsEmployee { get; set; }
+    }
+}
